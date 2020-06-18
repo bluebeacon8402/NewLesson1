9554dabe0b91e6e8cbe0340b4cb13ff8b7525df9
Chris Sorge <crsorge@icloud.com>
2020-06-18 15:39:24 -0400

    new assignment

---
+++ Declare and Initialize Lesson/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Declare_and_Initialize_Lesson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String myName = "Chris Sorge";
+            int myAge = 38;
+            char myGrade = 'A';
+            bool Happy = true;
+            double myMoney = 100.00;
+            decimal myLocation = 55500.0034M;
+
+            Console.WriteLine($"My name is{myName}, I am {myAge} years old");
+            Console.WriteLine($"I got an {myGrade} on my assignment");
+            Console.WriteLine($"It is very {Happy} that I enjoy eating food daily");
+            Console.WriteLine($"I currently have {myMoney} dollars in my pocket");
+            Console.WriteLine($"I was able to find {myLocation} donuts in my house");
+        }
+    }
+}
